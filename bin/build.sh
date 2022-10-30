9aadb2d9a419e4429e1fc2437f4528942b8cd271
David Bell <david@bell.id.au>
2022-10-30 15:53:52 +1100

    make build script more robust

--- bin/build.sh
+++ bin/build.sh
@@ -2,36 +2,47 @@
 die() { echo "${1:-nope}" >&2; exit "${2:-1}"; }
 
 # dep check
-# m=()
-# for d in go hub; do
-#     hash $d && continue
-#     m+=("$d")
-# done 2>/dev/null
-# (( ${#m[@]} > 0 )) && die "missing deps: ${m[*]}"
+m=()
+for d in git gh go; do
+    hash $d && continue
+    m+=("$d")
+done 2>/dev/null
+(( ${#m[@]} > 0 )) && die "missing deps: ${m[*]}"
+
+# what're we building
+tool=$(basename "$(git rev-parse --show-toplevel)")
 
 # make the build dir
 mkdir -p ./build
 
 # whats the ver
-# default version string is "edge"
-ver=edge
+# default version string is the branch name, short hash, and "edge"
+branch=$(git rev-parse --abbrev-ref HEAD)
+hash=$(git rev-parse --short HEAD)
+ver="$branch-$hash-edge"
 release=nope
-# if the CHANGELOG is in the current commit, we're releasing using the top line as the version
-git diff --quiet HEAD^ HEAD -- CHANGELOG || { release=yep; ver=$(head -n1 CHANGELOG);}
+# if the CHANGELOG is in the current commit, we're releasing
+git diff --quiet HEAD^ HEAD -- CHANGELOG || release=yep
+
+# release being `yep` only matters on main, we'll use the top line of CHANGELOG as the ver
+[[ $release == "yep" ]] && [[ $branch == "main" ]] && ver=$(head -n1 CHANGELOG)
 
 # lets go
 ld="-X main.version=$ver"
 for p in linux darwin darwin-arm windows; do
     arch=amd64
     [[ $p == darwin-arm ]] && { p=darwin; arch=arm64; }
-    fn=lash-$p-$arch-"$ver"
+    fn="$tool-$p-$arch-$ver"
     [[ $p == windows ]] && fn+=".exe"
 
     echo ":golang::$p: $fn"
     GOARCH=amd64 GOOS=$p CGO_ENABLED=0 \
-        go build -ldflags="$ld" -o ./build/$fn || die "cant build"
+        go build -ldflags="$ld" -o "./build/$fn" || die "cant build"
 done
 
-[[ $release == "nope" ]] && die "not releasing!" 0
+[[ $release == "nope" ]] && die "built successfully, not releasing!" 0
+
+# don't publish releases from branches
+[[ $branch == "main" ]] || die "oops, we only release on main" 1
 
 gh release create "$ver" build/*
