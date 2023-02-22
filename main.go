392189d7e04325dad03c0c3e6d93e15295fc7b66
David Bell <david@bell.id.au>
2023-02-22 21:26:43 +1100

    start adding url flag

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -70,6 +73,16 @@ type badge struct {
 	role string // role name
 }
 
+type sessionToken struct {
+	SessionID    string `json:"sessionId"`
+	SessionKey   string `json:"sessionKey"`
+	SessionToken string `json:"sessionToken"`
+}
+
+type federationResponse struct {
+	SigninToken string
+}
+
 func main() {
 	homedir, err := os.UserHomeDir()
 	if err != nil {
@@ -83,6 +96,7 @@ func main() {
 	finit := flag.Bool("init", false, "make the lash sub-directory and re-create the config.json file")
 	fnonick := flag.Bool("n", false, "disable nicknames")
 	frefresh := flag.Bool("r", false, "refresh caches (token and profiles)")
+	furl := flag.Bool("u", false, "generate an aws console url for the chosen role")
 	fver := flag.Bool("v", false, "print program version")
 	flag.Parse()
 
@@ -200,6 +214,66 @@ fuzzy:
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "cant get keys for %s: %v\n", choice, err)
 		os.Exit(5)
+	}
+
+	if *furl {
+		fedUrl := "https://signin.aws.amazon.com/federation"
+		t := sessionToken{
+			SessionID:    keys["AccessKeyId"],
+			SessionKey:   keys["SecretAccessKey"],
+			SessionToken: keys["SessionToken"],
+		}
+		b, err := json.Marshal(t)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cant marshal keys: %v\n", err)
+			os.Exit(1)
+		}
+
+		req, err := http.NewRequest("GET", fedUrl, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cant create federation request: %v\n", err)
+			os.Exit(1)
+		}
+
+		qf := req.URL.Query()
+		qf.Add("Action", "getSigninToken")
+		qf.Add("SessionType", "json")
+		qf.Add("Session", string(b))
+		req.URL.RawQuery = qf.Encode()
+		fmt.Fprintf(os.Stderr, "url: %s\n", req.URL.String())
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to get federation response: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cant read federation response body: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stderr, "%s\n", body)
+		var fedResp federationResponse
+		err = json.Unmarshal(body, &fedResp)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cant unmarshal federation response body: %v\n", err)
+			os.Exit(1)
+		}
+		signinToken := fedResp.SigninToken
+
+		u, err := url.Parse(fedUrl)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cant parse sign in url: %v\n", err)
+			os.Exit(1)
+		}
+		qs := u.Query()
+		qs.Set("SigninToken", signinToken)
+		qs.Set("Destination", fmt.Sprintf("https://%s.console.aws.amazon.com/", cfg.Region))
+		qs.Set("Action", "login")
+		u.RawQuery = qs.Encode()
+		fmt.Fprintf(os.Stdout, "%s\n", u.String())
+		os.Exit(0)
 	}
 
 	// write the credentials file and exit zero
